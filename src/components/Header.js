c15c3a850b6000cd3736576e7ea361e2f116ff36
zohaibasif121 <138899712+zohaibasif121@users.noreply.github.com>
2024-08-30 18:32:22 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import { v4 as uuidv4 } from "uuid";
 
 function Header(props) {
-
+const REF=useRef(null);
     function handleInput(event) {
         props.setinput(event.target.value);
     }
@@ -15,7 +15,7 @@ function Header(props) {
             const newArr = [...props.Task, { id:uuidv4() , task: props.input ,check:false }];
             props.set(newArr);
             props.setinput("");
-            
+            REF.current.focus();
         }
     }
 
@@ -27,6 +27,7 @@ function Header(props) {
         
         <input type='text' placeholder='Enter youe Tasks'
         value={props.input} onChange={handleInput}
+        ref={REF}
         />
         <button type='submit'
         onClick={handleADD}
